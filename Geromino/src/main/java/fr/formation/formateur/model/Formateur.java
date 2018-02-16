30a17d36f8c637a66867208187083c7873b99afe
Bin <clement.bin@hotmail.fr>
2018-02-16 10:10:55 +0100

    plop

--- Geromino/src/main/java/fr/formation/formateur/model/Formateur.java
+++ Geromino/src/main/java/fr/formation/formateur/model/Formateur.java
@@ -9,4 +9,32 @@ public class Formateur {
 	private double patience;
 	private List<Date> disponibilite;
 	private HashMap<Matiere, Niveau> expertise;
+	
+	
+	
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public double getPatience() {
+		return patience;
+	}
+	public void setPatience(double patience) {
+		this.patience = patience;
+	}
+	public List<Date> getDisponibilite() {
+		return disponibilite;
+	}
+	public void setDisponibilite(List<Date> disponibilite) {
+		this.disponibilite = disponibilite;
+	}
+	public HashMap<Matiere, Niveau> getExpertise() {
+		return expertise;
+	}
+	public void setExpertise(HashMap<Matiere, Niveau> expertise) {
+		this.expertise = expertise;
+	}
+	
 }
