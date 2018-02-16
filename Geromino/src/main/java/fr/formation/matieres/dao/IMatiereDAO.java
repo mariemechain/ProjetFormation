50d0a60272f76cb03e20dc706a72cd927c3ba472
Unknown <klervi29@hotmail.com>
2018-02-16 09:50:13 +0100

    Matiere DAO

---
+++ Geromino/src/main/java/fr/formation/matieres/dao/IMatiereDAO.java
@@ -0,0 +1,14 @@
+package fr.formation.matieres.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import fr.formation.matieres.model.*;
+
+public interface IMatiereDAO extends JpaRepository<Matiere, Integer>{
+
+	@Query("select matiere from Matiere matiere where matiere.titre = :titre")
+	public Matiere findByTitre(@Param("titre") String titre);
+	
+}
