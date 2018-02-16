225091e29444b1379a9fb370cff54f9a5ea6f591
LéoMallet <leomalletdida@gmail.com>
2018-02-16 14:15:22 +0100

    VideoProjecteur.java ajout du joinColumn

--- Geromino/src/main/java/fr/formation/ressources/metier/VideoProjecteur.java
+++ Geromino/src/main/java/fr/formation/ressources/metier/VideoProjecteur.java
@@ -2,6 +2,7 @@ package fr.formation.ressources.metier;
 
 import javax.persistence.Cacheable;
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
@@ -11,6 +12,7 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 @Table(name="videoprojecteur")
 @Cacheable
 @Cache(usage=CacheConcurrencyStrategy.READ_WRITE)
+@PrimaryKeyJoinColumn(name="VID_ID", referencedColumnName="MAT_ID")
 public class VideoProjecteur extends Materiel {
 
 	
