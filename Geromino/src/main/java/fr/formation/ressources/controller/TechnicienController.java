034402946ddc61c8778203ac6e51b61f00db2360
Unknown <hustache.julie@orange.fr>
2018-02-16 12:20:43 +0100

    TechnicienCOntroller + layout

--- Geromino/src/main/java/fr/formation/ressources/controller/TechnicienController.java
+++ Geromino/src/main/java/fr/formation/ressources/controller/TechnicienController.java
@@ -1,12 +1,23 @@
 package fr.formation.ressources.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.formation.ressources.dao.IOrdinateurDAO;
+import fr.formation.ressources.dao.ITechnicienDAO;
+import fr.formation.ressources.dao.IVideoProjecteurDAO;
 
 @Controller
 @RequestMapping("/technicien")
 public class TechnicienController {
 
+	@Autowired
+	private ITechnicienDAO techDAO;
+	@Autowired
+	private IOrdinateurDAO ordiDAO;
+	@Autowired
+	private IVideoProjecteurDAO videoDAO;
 	
 	
 }
