dab8185eedb1deb99b17da9383ff255c890d49ac
Unknown <ndoriyece@gmail.com>
2018-02-19 10:25:29 +0100

    Commit: Gestion des erreurs HibernateObjectMapper
    [Add] Classe HibernateObjectMapper au sein du package .config
    [UPDATE] Mention du package de la classe dans api-context.xml

---
+++ Geromino/src/main/java/fr/formation/config/HibernateObjectMapper.java
@@ -0,0 +1,13 @@
+package fr.formation.config;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+
+public class HibernateObjectMapper extends ObjectMapper {
+
+	    private static final long serialVersionUID = 1L;
+	    
+	    public HibernateObjectMapper() {
+	        this.registerModule(new Hibernate5Module());
+	    }
+	
+}
