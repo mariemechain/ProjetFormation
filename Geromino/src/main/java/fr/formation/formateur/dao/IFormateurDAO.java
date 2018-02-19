0cf1f3f5833e28aa8ca50f804a74db0f57047abf
Bin <clement.bin@hotmail.fr>
2018-02-19 10:22:11 +0100

    La methode de recherche dans IFormateur est ok

--- Geromino/src/main/java/fr/formation/formateur/dao/IFormateurDAO.java
+++ Geromino/src/main/java/fr/formation/formateur/dao/IFormateurDAO.java
@@ -1,9 +1,15 @@
 package fr.formation.formateur.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import fr.formation.formateur.model.Formateur;
+import fr.formation.formateur.model.Niveau;
 
 public interface IFormateurDAO extends JpaRepository<Formateur, Integer>{
-
+	
+	@Query("select f from Formateur f left join f.expertises e where e.matiere.id =:matiere and e.niveau >=:niveau")
+	public Formateur findByFilter(@Param("matiere") int matiere, @Param("niveau") Niveau niveau);
+	
 }
