c315d4571bb173a1c85d864f7b0f98a00090b09c
Bin <clement.bin@hotmail.fr>
2018-02-20 12:21:00 +0100

    j'ai plus dis D

---
+++ Geromino/src/main/java/fr/formation/formateur/dao/IFormateurDAO.java
@@ -0,0 +1,11 @@
+package fr.formation.formateur.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.formation.formateur.model.Formateur;
+
+public interface IFormateurDAO extends JpaRepository<Formateur, Integer>{
+	
+
+	
+}
