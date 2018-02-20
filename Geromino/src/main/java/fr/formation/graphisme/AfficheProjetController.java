c3171b848914c139f7cb12d4195c7683d4ece229
Bin <clement.bin@hotmail.fr>
2018-02-20 12:16:35 +0100

    plep

--- Geromino/src/main/java/fr/formation/graphisme/AfficheProjetController.java
+++ Geromino/src/main/java/fr/formation/graphisme/AfficheProjetController.java
@@ -1,5 +1,33 @@
 package fr.formation.graphisme;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import fr.formation.matieres.dao.IMatiereDAO;
+import fr.formation.projets.model.Projet;
+
+@Controller
+@RequestMapping(value = "/afficheProjet")
 public class AfficheProjetController {
 
+	@Autowired
+	private IMatiereDAO daoMat;
+	
+	@Autowired
+	private IProjetDAO daoPro;
+	
+	@Autowired
+	private IFormateurDAO daoFor;
+	
+	@Autowired
+	private IPlanificationDAO daoPla;
+	
+	@GetMapping("")
+	public String afficher(@RequestParam("id") int id, Projet projet) {
+		projet.addAttribute("projet", daoPro.findById(id).get());
+		return "afficheProjet";
+	}
 }
