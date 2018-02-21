2281c4db8caeb8f849aefebea66cb620c5330719
Corentin Jayer <cojay@hotmail.fr>
2018-02-21 16:09:01 +0100

    upload fonctionnel (seul au monde)

--- Geromino/src/main/java/fr/formation/matieres/controller/UploadController.java
+++ Geromino/src/main/java/fr/formation/matieres/controller/UploadController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,7 +28,7 @@ public class UploadController {
         return "Upload";
     }
      
-    @RequestMapping(value = "/doUpload", method = RequestMethod.POST)
+    @PostMapping("/upload/doUpload")
     public String handleFileUpload(HttpServletRequest request,
             @RequestParam CommonsMultipartFile[] fileUpload) throws Exception {
           
