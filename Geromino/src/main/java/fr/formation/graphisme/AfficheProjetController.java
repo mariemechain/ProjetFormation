21cbbe7b6314ccdc543c6f8724d2acfb925c1d07
Bin <clement.bin@hotmail.fr>
2018-02-22 13:19:12 +0100

    tite aide

--- Geromino/src/main/java/fr/formation/graphisme/AfficheProjetController.java
+++ Geromino/src/main/java/fr/formation/graphisme/AfficheProjetController.java
@@ -46,126 +46,76 @@ public class AfficheProjetController {
 		List<String> jours = new ArrayList<String>();
 		List<Integer> duree_mois = new ArrayList<Integer>();
 		List<String> mois = new ArrayList<String>();
-		duree_mois.add(0);
+
 		for (int i = 0; i < projet.getDuree(); i++) {
-			String res = "";
-			int res2 = 0;
-			dates.add((Date) java.sql.Date.valueOf(local_date.plusDays(i)));
-			switch (dates.get(i).getDay()) {
-			case 0:
-				res = "Dimanche";
-				break;
-			case 1:
-				res = "Lundi";
-				
-				break;
-			case 2:
-				res = "Mardi";
-				break;
-			case 3:
-				res = "Mercredi";
-				break;
-			case 4:
-				res = "Jeudi";
-				break;
-			case 5:
-				res = "Vendredi";
-				break;
-			case 6:
-				res = "Samedi";
-				break;
-			}
-			jours.add(res);
-			switch (dates.get(i).getMonth()) {
-			case 0:
-				res = "Janvier";
-				res2 = 31;
-				break;
-			case 1:
-				res = "Février";
-				if ((date.getYear() + 1900) % 400 == 0) {
-					res2 = 29;
-				} else if ((date.getYear() + 1900) % 100 == 0) {
-					res2 = 28;
-				} else if ((date.getYear() + 1900) % 4 == 0) {
-					res2 = 29;
-				} else {
-					res2 = 28;
-				}
-				break;
-			case 2:
-				res = "Mars";
-				res2 = 31;
-				break;
-			case 3:
-				res = "Avril";
-				res2 = 30;
-				break;
-			case 4:
-				res = "Mai";
-				res2 = 31;
-				break;
-			case 5:
-				res = "Juin";
-				res2 = 30;
-				break;
-			case 6:
-				res = "Juillet";
-				res2 = 31;
-				break;
-			case 7:
-				res = "Aout";
-				res2 = 31;
-				break;
-			case 8:
-				res = "Septembre";
-				res2 = 30;
-				break;
-			case 9:
-				res = "Octobre";
-				res2 = 31;
-				break;
-			case 10:
-				res = "Novembre";
-				res2 = 30;
-				break;
-			case 11:
-				res = "Decembre";
-				res2 = 31;
-				break;
-			}
-			mois.add(res);
-			duree_mois.add(res2+duree_mois.get(i));
+			jours.add(nom_day(i, dates, date, local_date));
+			mois.add(nom_month(i, dates, date));
+			duree_mois.add(duree_month(i, dates, date));
 		}
-		
+
 		Date date_fin = dates.get(projet.getDuree() - 1);
 		int moi = date.getMonth();
+
 		if (date_fin.getMonth() == moi) {
-			duree_mois.set(1, date_fin.getDate()-date.getDate());
+			duree_mois.set(0, date_fin.getDate() - date.getDate() + 1);
 		} else {
-			duree_mois.set(1, duree_mois.get(1)-date.getDate());
-			duree_mois.set(date_fin.getMonth()-date.getMonth()+1, date_fin.getDate()+duree_mois.get(date_fin.getMonth()-date.getMonth()));
+			duree_mois.set(0, duree_mois.get(1) - date.getDate() + 1);
+			duree_mois.set(projet.getDuree() - 1, date_fin.getDate());
+		}
+		for (int i = 1; i < duree_mois.get(0); i++) {
+			duree_mois.set(i, duree_mois.get(0));
+		}
+		for (int i = 1; i < duree_mois.get(projet.getDuree() - 1); i++) {
+			duree_mois.set(projet.getDuree() - 1 - i, date_fin.getDate());
 		}
 		
-		
+		List<Integer> duree_mois_tot = new ArrayList<Integer>();
+		int nb_mois = date_fin.getMonth() - date.getMonth() + 1;
+
+		List<Integer> duree_mois_base = new ArrayList<Integer>();
+		duree_mois_base.add(duree_mois.get(0));
+		for (int i = 1; i < nb_mois; i++) {
+			duree_mois_base.add(duree_month(
+					((Date) java.sql.Date.valueOf(((java.sql.Date) projet.getDebut()).toLocalDate().plusMonths(i)))));
+			if (i == nb_mois - 1) {
+				duree_mois_base.set(i, date_fin.getDate());
+			}
+		}
+		System.out.println(duree_mois_base);
+		System.out.println("_________________");
+		System.out.println(duree_mois);
+		for (int i = 0; i < duree_mois.get(0); i++) {
+			duree_mois_tot.add(0);
+		}
+		System.out.println("_________________");
+		System.out.println(duree_mois_tot);
+		for (int i = 1; i < nb_mois; i++) {
+			for (int j = 0; j < duree_mois_base.get(i); j++) {
+				duree_mois_tot.add(duree_mois_tot.get(duree_mois_base.get(i-1)-(j+1)) + duree_mois_base.get(i - 1));
+			}
+		}
+
+		System.out.println(duree_mois_tot);
+
 		List<String> matieres = new ArrayList<String>();
 		List<Integer> matieres_duree = new ArrayList<Integer>();
-		for(int i=0; i<projet.getPlanifications().size();i++){
+		for (int i = 0; i < projet.getPlanifications().size(); i++) {
 			matieres.add(projet.getPlanifications().get(i).getMatiere().getTitre());
 			matieres_duree.add(projet.getPlanifications().get(i).getMatiere().getDuree());
 		}
-		
+
 		List<Integer> long_rawspawn = new ArrayList<Integer>();
-		for (int i = 0; i<projet.getDuree();i++){
-			
+		for (int i = 0; i < projet.getDuree(); i++) {
+
 		}
 		model.addAttribute("dates", dates);
-		
+
 		model.addAttribute("jours", jours);
-		
+
 		model.addAttribute("mois", mois);
 		model.addAttribute("duree_mois", duree_mois);
-		
+		model.addAttribute("duree_mois_tot", duree_mois_tot);
+
 		model.addAttribute("matieres", matieres);
 		model.addAttribute("matieres_duree", matieres_duree);
 
@@ -174,5 +124,185 @@ public class AfficheProjetController {
 		model.addAttribute("date", projet.getDebut().getDate());
 
 		return "afficheProjet";
+	}
+
+	@SuppressWarnings("deprecation")
+	private int duree_month(Date date) {
+		int res2 = 0;
+		switch (date.getMonth()) {
+		case 0:
+			res2 = 31;
+			break;
+		case 1:
+			if ((date.getYear() + 1900) % 400 == 0) {
+				res2 = 29;
+			} else if ((date.getYear() + 1900) % 100 == 0) {
+				res2 = 28;
+			} else if ((date.getYear() + 1900) % 4 == 0) {
+				res2 = 29;
+			} else {
+				res2 = 28;
+			}
+			break;
+		case 2:
+			res2 = 31;
+			break;
+		case 3:
+			res2 = 30;
+			break;
+		case 4:
+			res2 = 31;
+			break;
+		case 5:
+			res2 = 30;
+			break;
+		case 6:
+			res2 = 31;
+			break;
+		case 7:
+			res2 = 31;
+			break;
+		case 8:
+			res2 = 30;
+			break;
+		case 9:
+			res2 = 31;
+			break;
+		case 10:
+			res2 = 30;
+			break;
+		case 11:
+			res2 = 31;
+			break;
+		}
+		return res2;
+	}
+
+	@SuppressWarnings("deprecation")
+	private int duree_month(int id, List<Date> dates, Date date) {
+		int res2 = 0;
+		switch (dates.get(id).getMonth()) {
+		case 0:
+			res2 = 31;
+			break;
+		case 1:
+			if ((date.getYear() + 1900) % 400 == 0) {
+				res2 = 29;
+			} else if ((date.getYear() + 1900) % 100 == 0) {
+				res2 = 28;
+			} else if ((date.getYear() + 1900) % 4 == 0) {
+				res2 = 29;
+			} else {
+				res2 = 28;
+			}
+			break;
+		case 2:
+			res2 = 31;
+			break;
+		case 3:
+			res2 = 30;
+			break;
+		case 4:
+			res2 = 31;
+			break;
+		case 5:
+			res2 = 30;
+			break;
+		case 6:
+			res2 = 31;
+			break;
+		case 7:
+			res2 = 31;
+			break;
+		case 8:
+			res2 = 30;
+			break;
+		case 9:
+			res2 = 31;
+			break;
+		case 10:
+			res2 = 30;
+			break;
+		case 11:
+			res2 = 31;
+			break;
+		}
+		return res2;
+	}
+
+	@SuppressWarnings("deprecation")
+	private String nom_month(int id, List<Date> dates, Date date) {
+
+		String res = "";
+		switch (dates.get(id).getMonth()) {
+		case 0:
+			res = "Janvier";
+			break;
+		case 1:
+			res = "Février";
+			break;
+		case 2:
+			res = "Mars";
+			break;
+		case 3:
+			res = "Avril";
+			break;
+		case 4:
+			res = "Mai";
+			break;
+		case 5:
+			res = "Juin";
+			break;
+		case 6:
+			res = "Juillet";
+			break;
+		case 7:
+			res = "Aout";
+			break;
+		case 8:
+			res = "Septembre";
+			break;
+		case 9:
+			res = "Octobre";
+			;
+			break;
+		case 10:
+			res = "Novembre";
+			break;
+		case 11:
+			res = "Decembre";
+			break;
+		}
+		return res;
+	}
+
+	@SuppressWarnings("deprecation")
+	private String nom_day(int id, List<Date> dates, Date date, LocalDate local_date) {
+		String res = "";
+		dates.add((Date) java.sql.Date.valueOf(local_date.plusDays(id)));
+		switch (dates.get(id).getDay()) {
+		case 0:
+			res = "Dimanche";
+			break;
+		case 1:
+			res = "Lundi";
+			break;
+		case 2:
+			res = "Mardi";
+			break;
+		case 3:
+			res = "Mercredi";
+			break;
+		case 4:
+			res = "Jeudi";
+			break;
+		case 5:
+			res = "Vendredi";
+			break;
+		case 6:
+			res = "Samedi";
+			break;
+		}
+		return res;
 	}
 }
--- Geromino/src/main/webapp/WEB-INF/views/afficheProjet.jsp
+++ Geromino/src/main/webapp/WEB-INF/views/afficheProjet.jsp
@@ -21,23 +21,22 @@
 				<tbody>
 					<c:forEach var="i" begin="0" end="${duree-1}">
 						<tr>
-							<c:choose>
-								<c:when test="${i==0}">
+							<c:if test="${i==duree_mois_tot[i]}">
+								<td rowspan="${duree_mois[i]}">${mois[i]}</td>
+							</c:if>
+							<%-- 	<c:when test="${i==duree_mois[i]+1}">
+									<td rowspan="${duree_mois[i+1]}">${mois[i]}</td>
+								</c:when>
+								<c:when test="${i==duree_mois[i]+1}">
 									<td rowspan="${duree_mois[i+1]+1}">${mois[i]}</td>
 								</c:when>
-								<c:when test="${i==duree_mois[1]+1}">
+								<c:when test="${i==duree_mois[i]+1}">
 									<td rowspan="${duree_mois[i+1]+1}">${mois[i]}</td>
 								</c:when>
-								<c:when test="${i==duree_mois[2]+1}">
+								<c:when test="${i==duree_mois[i]+1}">
 									<td rowspan="${duree_mois[i+1]+1}">${mois[i]}</td>
-								</c:when>
-								<c:when test="${i==duree_mois[3]+1}">
-									<td rowspan="${duree_mois[i+1]+1}">${mois[i]}</td>
-								</c:when>
-								<c:when test="${i==duree_mois[4]+1}">
-									<td rowspan="${duree_mois[i+1]+1}">${mois[i]}</td>
-								</c:when>
-							</c:choose>
+								</c:when> --%>
+
 							<td>${dates[i].getDate()}</td>
 							<td>${jours[i]}</td>
 							<c:if test="${i==0}">
@@ -47,14 +46,19 @@
 								<td rowspan="${matieres_duree[i]}">PAPA</td>
 								<td rowspan="${matieres_duree[i]}">MAMAN</td>
 							</c:if>
+								<c:if test="${i==matieres_duree[0]}">
+								<td rowspan="${matieres_duree[i]}">${matieres[i]}</td>
+							</c:if>
+							<c:if test="${i==matieres_duree[0]}">
+								<td rowspan="${matieres_duree[i]}">PAPA</td>
+								<td rowspan="${matieres_duree[i]}">MAMAN</td>
+							</c:if>
 						</tr>
 					</c:forEach>
 				</tbody>
 			</table>
 		</div>
-		<div>
-			Gros bouton export PDF
-		</div>
+		<div>Gros bouton export PDF</div>
 	</tiles:putAttribute>
 </tiles:insertDefinition>
 
